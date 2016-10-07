3642823aae1d8db53c21a98bc80d7a69f4ef5f28
Usmanov, Pavel (CORP) <Pavel.Usmanov@adp.com>
2016-10-07 11:25:51 -0400

    Prod mode and icons

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
-System.register(['@angular/platform-browser-dynamic', './components/quizapp.component'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/platform-browser-dynamic', './components/quizapp.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var platform_browser_dynamic_1, quizapp_component_1;
+    var core_1, platform_browser_dynamic_1, quizapp_component_1;
     return {
         setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
             function (platform_browser_dynamic_1_1) {
                 platform_browser_dynamic_1 = platform_browser_dynamic_1_1;
             },
@@ -11,6 +14,7 @@ System.register(['@angular/platform-browser-dynamic', './components/quizapp.comp
                 quizapp_component_1 = quizapp_component_1_1;
             }],
         execute: function() {
+            core_1.enableProdMode();
             platform_browser_dynamic_1.bootstrap(quizapp_component_1.QuizApp);
         }
     }
--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,7 @@
+import {Component, enableProdMode} from '@angular/core';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {QuizApp} from './components/quizapp.component';
+
+enableProdMode();
 
 bootstrap(QuizApp);
