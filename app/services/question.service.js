99279db06b6b7f5b5c458eb93dedbf60af0c9e10
Pavel Usmanov <pavel.usmanov@gmail.com>
2017-02-04 13:00:59 -0500

    Removed node_modules

--- app/services/question.service.js
+++ app/services/question.service.js
@@ -24,7 +24,8 @@ var QuizService = (function () {
 }());
 QuizService = __decorate([
     core_1.Injectable(),
-    __metadata("design:paramtypes", [http_1.Http])
+    __metadata("design:paramtypes", [typeof (_a = typeof http_1.Http !== "undefined" && http_1.Http) === "function" && _a || Object])
 ], QuizService);
 exports.QuizService = QuizService;
+var _a;
 //# sourceMappingURL=question.service.js.map
