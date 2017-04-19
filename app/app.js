c1774cbc31e1056b416c5ac9372ba0a946904788
Pavel Usmanov <pavel.usmanov@gmail.com>
2017-04-19 10:22:33 -0400

    Reduced bundle and included jquery separately

--- app/app.js
+++ app/app.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-require("jquery");
-require("materialize-css");
-require("angular2-materialize");
+// If you want to bundle these - uncomment (results in larger file)
+// import 'jquery';
+// import 'materialize-css';
+// import 'angular2-materialize';
 var app_module_1 = require("./modules/app.module");
 core_1.enableProdMode();
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule)
--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,9 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import 'jquery';
-import 'materialize-css';
-import 'angular2-materialize';
+// If you want to bundle these - uncomment (results in larger file)
+// import 'jquery';
+// import 'materialize-css';
+// import 'angular2-materialize';
 import {AppModule} from './modules/app.module';
 
 enableProdMode();
